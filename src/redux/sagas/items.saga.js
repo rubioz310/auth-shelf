be8c740499358601585750066db31a434f009213
Meghan Cleland <mplscleland@gmail.com>
2021-07-30 10:32:20 -0500

    completed sagas and reducers

---
+++ src/redux/sagas/items.saga.js
@@ -0,0 +1,35 @@
+import { takeEvery, put } from "redux-saga/effects";
+import axios from "axios";
+
+function* itemSaga() {
+  yield takeEvery("GET_ITEMS", fetchAllItems);
+  yield takeEvery("ADD_ITEM", addItem);
+  yield takeEvery("DELETE_ITEM", deleteItem);
+}
+
+function* fetchAllItems() {
+  try {
+    const items = yield axios.get("/api/shelf");
+    console.log("get all items:", items.data);
+    yield put({ type: "SET_ITEMS", payload: items.data });
+  } catch {
+    console.log("get all items error");
+  }
+}
+
+function* addItem(action) {
+  yield axios.post("api/item", action.payload);
+  yield put({ type: "GET_ITEMS" });
+}
+//need to solve how to do delete
+
+function* deleteItem(action) {
+  try {
+    yield axios.delete(`/item/${action.payload}`);
+    yield put({ type: "GET_ITEMS" });
+  } catch {
+    console.log("Error deleting item");
+  }
+}
+
+export default itemSaga;
