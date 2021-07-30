e7f4507b67e281f7c54354fc93e4bbbf3d4fb76a
Jose Rubio <ru310z@gmail.com>
2021-07-30 10:12:06 -0500

    Created GET, POST and DELETE routers for items in shelf

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,28 +1,56 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
+const { 
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const { Pool } = require('pg');
+//all routes are using rejectUnauthenticated 
 /**
- * Get all of the items on the shelf
+ * Get all of the items on the shelf 
  */
-router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+router.get('/', rejectUnauthenticated, (req, res) => {
+  const query = `SELECT * FROM "item"`;
+  pool.query(query)
+  .then((results) => res.send(results.rows))
+  .catch((error) => {
+    console.log('Error getting all items: ', error);
+    res.sendStatus(500);
+  })
 });
 
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
-  // endpoint functionality
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const item = req.body;
+  const query = `insert into "item"("description","image_url","user_id")
+	values ($1, $2, $3);`
+
+  pool.query(query, [item.description, item.image_url, req.user.id])
+  .then(results => res.sendStatus(201))
+  .catch(error => {
+    console.log("Error adding item: ", error);
+    res.sendStatus(500);
+  })
 });
 
 /**
  * Delete an item if it's something the logged in user added
  */
-router.delete('/:id', (req, res) => {
-  // endpoint functionality
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM "item"
+                  WHERE "id" = $1 AND "user_id" = $2;`;
+    
+  pool.query(query, [id, req.user.id])
+  .then(results => res.sendStatus(202))
+  .catch(error => {
+    console.log("Error deleting item: ", error);
+    res.sendStatus(500);
+  })
 });
-
+// ******* STRETCH GOALS ************
 /**
  * Update an item if it's something the logged in user added
  */
---
+++ src/redux/sagas/items.saga.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App.js';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* itemSaga(){
+    yield takeEvery('GET_ITEMS', fetchAllItems);
+    
+}
+
+function* fetchAllItems(action) {
+    const items = yield axios.get('/api/shelf')
+}
